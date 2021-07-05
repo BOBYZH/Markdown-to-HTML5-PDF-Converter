258dedef53102180f65dce9aef6d7312c29e975d
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-07-05 08:54:11 +0000

    Update to convert from file

---
+++ README.md
@@ -0,0 +1,12 @@
+# Title
+(descripton)
+
+[link](https://replit.com/@BOBHYZ/Markdown-to-HTML5-PDF-Converter#README.md)
+
+## Subtitle 1
+- note
+
+## Subtitle 2
+1. first
+2. second
+3. third
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-// 
+// Set showdown and fs
 const showdown = require('showdown')
 const converter = new showdown.Converter()
 
-const md = '# hello, markdown!'
-const html = converter.makeHtml(md)
+const fs = require("fs")
 
-console.log('MD: ', md)
-console.log('HTML: ', html)
+// Define the path of markdown file
+const mdPath = './README.md' // default
+
+// Define variables of md and html
+const mdContent = fs.readFileSync(mdPath, "utf8")
+const htmlContent = converter.makeHtml(mdContent)
+
+// Show the input and output
+console.log('Markdown:')
+console.log(mdContent)
+console.log('')
+console.log('HTML:')
+console.log(htmlContent)
